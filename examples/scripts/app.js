52fdd9e346f1a917ba2d32af5967854e98079411
Maël Nison <nison.mael@gmail.com>
2013-01-25 19:32:15 -0500

    Improves example

--- examples/scripts/app.js
+++ examples/scripts/app.js
@@ -1,13 +1,60 @@
-window.onload = function ( ) { create( function ( scene, camera ) {
+var voxelManager, voxelEngine;
 
-    camera.position.set( 100, 100, 100 );
+var start = function ( ) {
+    var draw = false;
+
+    var queue = [ ];
+    var chrono;
+
+    var cast = function ( e ) {
+        var x = e.clientX, y = e.clientY;
+        var direction = new THREE.Vector3( ( x / width ) * 2 - 1, - ( y / height ) * 2 + 1, 1 );
+        var projector = new THREE.Projector( );
+        projector.unprojectVector( direction, camera );
+        direction.sub( camera.position ).normalize( );
+
+        var raycaster = new THREE.Raycaster( camera.position, direction, 0, 1000 );
+        var intersects = raycaster.intersectObjects( scene.children, true );
+
+        if ( intersects.length === 0 ) return ;
+
+        var voxel = intersects[ 0 ].point.clone( );
+        voxel.x = Math.floor( voxel.x + intersects[ 0 ].face.normal.x );
+        voxel.y = Math.floor( voxel.y + intersects[ 0 ].face.normal.y );
+        voxel.z = Math.floor( voxel.z + intersects[ 0 ].face.normal.z );
+
+        voxelEngine.set( voxel.x, voxel.y, voxel.z, 0x0000ff );
+
+        if ( ! chrono ) {
+            chrono = window.setTimeout( function ( ) {
+                voxelEngine.commit( );
+                chrono = undefined;
+            }, 100 );
+        }
+    };
+
+    $( document ).mousedown( function ( e ) {
+        draw = true; cast( e ); } );
+
+    $( document ).mouseup( function ( ) {
+        draw = false; } );
+
+    $( document ).mousemove( function ( e ) {
+        if ( ! draw ) return ;
+        cast( e );
+    } );
+};
+
+window.onload = function ( ) { create( function ( ) {
+
+    camera.position.set( 30, 30, 30 );
     camera.updateMatrix( );
     camera.lookAt( new THREE.Vector3( 0, 0, 0 ) );
 
-    var manager = new VOXEL.ThreeManager( );
-    scene.add( manager.object3D );
+    voxelManager = new VOXEL.ThreeManager( );
+    scene.add( voxelManager.object3D );
 
-    var voxelEngine = new VOXEL.VoxelEngine( manager );
+    voxelEngine = new VOXEL.VoxelEngine( voxelManager );
 
     for ( var x = - 50; x <= 50; ++ x )
         for ( var z = - 50; z <= 50; ++ z )
@@ -15,8 +62,12 @@ window.onload = function ( ) { create( function ( scene, camera ) {
 
     $( document.body ).addClass( 'ok' ).addClass( 'hard-loading' );
     voxelEngine.commit( function ( infos ) {
-        if ( infos.progress.success === infos.progress.total ) $( document.body ).removeClass( 'hard-loading' );
-        else $( '#loading .cursor' ).css( 'width', ( infos.progress.success / infos.progress.total * 100 ) + '%' );
+        if ( infos.progress.success === infos.progress.total ) {
+            $( document.body ).removeClass( 'hard-loading' );
+            start( );
+        } else {
+            $( '#loading .cursor' ).css( 'width', ( infos.progress.success / infos.progress.total * 100 ) + '%' );
+        }
     } );
 
 } ); };
--- examples/scripts/scene.js
+++ examples/scripts/scene.js
@@ -1,41 +1,45 @@
+var renderer, scene, camera, controls, light;
+var delta, width, height;
+
 function create( initializer, updater ) {
 
-    var renderer = new THREE.WebGLRenderer( {
+    renderer = new THREE.WebGLRenderer( {
         canvas : document.getElementById( 'screen' ),
         antialias : true
     } );
 
-    var scene = new THREE.Scene( );
+    scene = new THREE.Scene( );
 
-    var camera = new THREE.PerspectiveCamera( 60, 1, .001, 10000 );
+    camera = new THREE.PerspectiveCamera( 60, 1, .001, 10000 );
     scene.add( camera );
 
-    var controls = new THREE.FirstPersonControls( camera );
+    controls = new THREE.FirstPersonControls( camera );
     controls.movementSpeed = 10;
     controls.lookSpeed = 0.125;
     controls.lookVertical = true;
 
-    var light = new THREE.PointLight( 0xffffff );
+    light = new THREE.PointLight( 0xffffff );
     light.position = camera.position;
     scene.add( light );
 
     var clock = new THREE.Clock( );
 
     var updateScreen = window.onresize = function ( ) {
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        camera.aspect = window.innerWidth / window.innerHeight;
+        width = window.innerWidth, height = window.innerHeight;
+        renderer.setSize( width, height );
+        camera.aspect = width / height;
         camera.updateProjectionMatrix( );
     };
 
     var cycleRendering = function ( ) {
         window.requestAnimationFrame( cycleRendering );
-        var delta = clock.getDelta( );
-        if ( updater ) updater( delta );
+        delta = clock.getDelta( );
+        if ( updater ) updater( );
         //controls.update( delta );
         renderer.render( scene, camera );
     };
 
-    var returnValues = initializer( scene, camera );
+    var returnValues = initializer( );
     if ( returnValues ) updater = updater.bind.apply( updater, returnValues );
 
     updateScreen( );
