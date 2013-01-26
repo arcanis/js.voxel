ec2f8c5e573a55532baced6da3405918a08fefe7
Maël Nison <nison.mael@gmail.com>
2013-01-26 18:44:11 -0500

    Updates environment

--- utils/build.js
+++ utils/build.js
@@ -1,11 +1,7 @@
 var fs = require( 'fs' );
 
+var Glob = require( 'glob' );
 var UglifyJS = require( 'uglify-js' );
-
-var getSourceCode = function ( list ) {
-    return list.map( function ( path ) {
-        return fs.readFileSync( path, 'utf8' );
-    } ).join( '\n' ); };
 
 var uglifySourceCode = function ( sources ) {
     return UglifyJS
@@ -17,37 +13,5 @@ var uglifySourceCode = function ( sources ) {
                     REGION_DEPTH : 16
                 } } } ).code; };
 
-var clientSources = [
-
-    'src/client/namespace.js',
-
-    'src/client/Manager/ThreeManager.js',
-
-    'src/client/Loader/BinvoxLoader.js',
-
-    'src/client/Core/CreateWorker.js',
-    'src/client/Core/VoxelEngine.js'
-
-];
-
-var workerSources = [
-
-    'src/worker/namespace.js',
-
-    'src/worker/Algorithm/Tables.js',
-
-    'src/worker/Brush/BinvoxBrush.js',
-
-    'src/worker/Region/Region.js',
-    'src/worker/Region/RegionMap.js',
-
-    'src/worker/Core/Application.js',
-    'src/worker/Core/Main.js'
-
-];
-
-fs.writeFile( 'build/Voxel.js',
-    uglifySourceCode( clientSources ) +
-    'VOXEL.CreateWorker.dataScript = ' + JSON.stringify(
-        uglifySourceCode( workerSources ) ) +
-    ';' );
+fs.writeFileSync( 'build/Voxel.js',
+    uglifySourceCode( Glob.sync( 'src/**/*.js' ) ) );
